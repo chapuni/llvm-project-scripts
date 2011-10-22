7bf0f3aa6d0ed2c40d7920d536951f0772313019
NAKAMURA Takumi <geek4civic@gmail.com>
2011-10-22 09:11:24 +0900

    f2.pl: 思いっきり改造。JSON まで吐けるようになってる、かも。

--- f2.pl
+++ f2.pl
@@ -4,12 +4,24 @@ use FileHandle;
 use IPC::Open2;
 
 $verbose++ if grep(/VERBOSE/, @ARGV);
+$json++ if grep(/JSON/, @ARGV);
 
 $m_master = 'm/master';
 $t_master = 't/master';
 
-$tree = {};
-$subm = {};
+eval "require 'revs.pl'";
+
+@m_revs = sort {$a <=> $b} keys %dic_subm;
+
+if (scalar(@m_revs) != scalar(keys %dic_revs)) {
+	open($REVLOG, "> revs.pl") || die;
+	for my $r (@m_revs) {
+		print $REVLOG "\$dic_subm{$r}=\$s='$dic_subm{$r}';\$dic_tree{$r}=\$t='$dic_tree{$r}';\$dic_revs{\$s}=\$dic_revs{\$t}=$r;\n";
+	}
+	close($REVLOG);
+}
+
+open($REVLOG, ">> revs.pl") || die;
 
 # 現在の refs をすべて取得。
 open($F, "git show-ref |") || die;
@@ -24,29 +36,28 @@ while (<$F>) {
 	} elsif (m=^([0-9a-f]{40})\s+refs/heads/$m_master$=) {
 		# 各 submodule の最終 commit を得る。
 		$last{'m/master'} = $1;
-		open(my $F, "git ls-tree $1 |") || die;
-		while (<$F>) {
-			if (/^100644 blob ([0-9a-f]{40})\s+\.gitmodules$/) {
-				$subm->{'.gitmodules'}{A} = '100644';
-				$subm->{'.gitmodules'}{O} = 'blob';
-				$subm->{'.gitmodules'}{H} = $1;
-				$gitmodules = `git cat-file blob $1`;
-				next;
-			}
-			next unless my ($h, $repo) = /^160000 commit ([0-9a-f]{40})\s+(\S+)/;
-			$mins{$repo} = $h;
-			$subm->{$repo}{A} = '160000';
-			$subm->{$repo}{O} = 'commit';
-			$subm->{$repo}{H} = $h;
-			`git cat-file commit $h` =~ /^tree\s+([0-9a-f]{40})/;
-			$tree->{$repo}{A} = '040000';
-			$tree->{$repo}{O} = 'tree';
-			$tree->{$repo}{H} = $1
-		}
-		close($F);
 	}
 }
 close($F);
+
+sub readtree {
+	my ($commit) = @_;
+	$tree = {};
+	$subm = {};
+	return if $commit eq '';
+	open(my $F, "git ls-tree $commit |") || die;
+	while (<$F>) {
+		if (/^100644 blob ([0-9a-f]{40})\s+\.gitmodules$/) {
+			$subm->{'.gitmodules'}{B} = $1;
+			next;
+		}
+		next unless my ($h, $repo) = /^160000 commit ([0-9a-f]{40})\s+(\S+)/;
+		$subm->{$repo}{H} = $h;
+		`git cat-file commit $h` =~ /^tree\s+([0-9a-f]{40})/;
+		$tree->{$repo}{T} = $1;
+	}
+	close($F);
+}
 
 @dt = sort {$a <=> $b} keys %tags;
 if (@dt > 2048) {
@@ -56,32 +67,57 @@ if (@dt > 2048) {
 $parent = '';
 $parent_subm = '';
 
-# 新規のサブモジュールがあった場合、あきらめてぜんぶ取得するものとする。
-while (my ($repo, $h) = each %repos) {
-	next unless ($mins{$repo} eq '');
-	%mins = ();
-}
-
 @revs = ();
 
-while (my ($repo, $h) = each %repos) {
-	print STDERR "$repo=$h\n" if $verbose;
-	$commits{$repo} = &get_commits2($h, $mins{$repo});
-	push(@revs, keys %{$commits{$repo}});
-}
+########
 
-@t{@revs} = @revs;
-@revs = sort {$a <=> $b} keys %t;
+while (1) {
+	&readtree($last{'m/master'});
+
+	my $upd = 0;
+	while (my ($repo, $h) = each %repos) {
+		if ($h eq $subm->{$repo}{H}) {
+			print STDERR "$repo=$h (up-to-date)\n" if $verbose;
+			next;
+		}
+		print STDERR "$repo=$subm->{$repo}{H}..$h\n" if $verbose;
+		$commits{$repo} = {};
+		&get_commits2($commits{$repo}, $subm->{$repo}{H}, $h);
+		push(@revs, keys %{$commits{$repo}});
+		$upd++;
+		if ($subm->{$repo}{H} ne '') {
+			$repos{$repo} = $subm->{$repo}{H};
+		}
+	}
+
+	@t{@revs} = @revs;
+	@revs = sort {$a <=> $b} keys %t;
+
+	last if $upd == 0 || $last{'m/master'} eq '';
+
+	if ($revs[0] <= $m_revs[0]) {
+		$last{'m/master'} = '';
+		next;
+	}
+
+	# Great Linear Search
+	$i = $revs[0];
+	while (1) {
+		die "$i <= $m_revs[0]" if $i <= $m_revs[0];
+		next unless defined $dic_subm{--$i};
+		last;
+	}
+	print STDERR "r$i=$dic_subm{$i}\n" if $verbose;
+	$last{'m/master'} = $dic_subm{$i};
+}
 
 $parent_subm = $last{'m/master'};
 if ($parent_subm ne '') {
-	$lastrev = `git describe --tags $parent_subm`;
-	die unless $lastrev =~ m/^r(\d+)$/;
-	$parent = `git show-ref --hash t$1`;
+	$parent = $dic_tree{$dic_revs{$parent_subm}};
 }
 
+# 書きだす!
 for my $rev (@revs) {
-	my $orig_gitmodules = $gitmodules;
 	my $msg = '';
 	for my $repo (sort keys %repos) {
 		my $r = $commits{$repo}{$rev};
@@ -94,14 +130,8 @@ for my $rev (@revs) {
 				$msg = $v;
 			}
 		}
-		$tree->{$repo}{A} = '040000';
-		$tree->{$repo}{O} = 'tree';
-		$tree->{$repo}{H} = $r->{tree};
-		if (!defined $subm->{$repo}) {
-			$gitmodules .= "[submodule \"$repo\"]\n\tpath = $repo\n\turl = http://llvm.org/git/$repo.git\n";
-		}
-		$subm->{$repo}{A} = '160000';
-		$subm->{$repo}{O} = 'commit';
+		&update_gitmodules($subm, $repo) if (!defined $subm->{$repo}{H});
+		$tree->{$repo}{T} = $r->{tree};
 		$subm->{$repo}{H} = $r->{commit};
 	}
 
@@ -109,18 +139,12 @@ for my $rev (@revs) {
 	$parent = &make_commit($tree, $msg, $parent);
 	print STDERR "t $parent $rev $ENV{GIT_AUTHOR_NAME}\n" if $verbose;
 
-	# Submodule
-	if ($gitmodules ne $orig_gitmodules) {
-		my $gm = sub {
-			my ($F) = @_;
-			print $F $gitmodules;
-		};
-		$subm->{'.gitmodules'}{A} = '100644';
-		$subm->{'.gitmodules'}{O} = 'blob';
-		$subm->{'.gitmodules'}{H} = &sb_hash("git hash-object -w --stdin", $gm);
-	}
 	$parent_subm = &make_commit($subm, $msg, $parent_subm);
 	print STDERR "m $parent_subm $rev $ENV{GIT_AUTHOR_NAME}\n" if $verbose;
+
+	print $REVLOG "\$dic_subm{$rev}=\$s='$parent_subm';";
+	print $REVLOG "\$dic_tree{$rev}=\$t='$parent';";
+	print $REVLOG "\$dic_revs{\$s}=\$dic_revs{\$t}=$rev;\n";
 
 	if ((++$nrevs & 255) == 0 || $rev >= $revs[$#revs - 100]) {
 		system("git update-ref refs/tags/t$rev $parent") && die;
@@ -129,27 +153,80 @@ for my $rev (@revs) {
 		system("git update-ref refs/heads/m/master $parent_subm") && die;
 	}
 
+	if ($json) {
+		my %js = (branch=>'master',
+			   project=>'llvm-project');
+		$js{'revision'} = "r$rev";
+		$js{'repository'} = 'git://github.com/chapuni/llvm-project';
+		$ENV{GIT_AUTHOR_DATE} =~ /^(\d+)/;
+		$js{'when'} = $1;
+		$js{'who'} = sprintf("%s <%s>",
+							 $ENV{GIT_AUTHOR_NAME},
+							 $ENV{GIT_AUTHOR_EMAIL});
+		$js{'comments'} = $msg;
+
+		open(my $df, "git diff-tree --numstat $parent |");
+		$js{'files'} = '['.join(',',join(',', grep(s=^\d+\s+\d+\s+(.*)\r*\n*$=\"\1\"=, <$df>))).']';
+		close($df);
+
+		for (keys %js) {
+			my @a = split(//, $js{$_});
+			for (@a) {
+				if ($_ eq ' ') {
+					$_ = '+';
+				} elsif (/^\r*\n$/) {
+					$_ = '%0A';
+				} elsif (/^[-0-9A-Z_a-z]$/) {
+				} else {
+					$_ = sprintf('%%%02X', ord);
+				}
+			}
+			$js{$_} = join('', @a);
+		}
+		open(my $fj, "> _.bak") || die;
+		print $fj join('&', map {"$_=$js{$_}"} sort keys %js);
+		close($fj);
+		system('wget http://bb.pgr.jp/change_hook/base --post-file=_.bak');
+	}
 }
 
+close($REVLOG);
+
 exit;
+
+sub update_gitmodules {
+	my ($subm, $repo) = @_;
+	my $gm = $subm->{'.gitmodules'};
+	$gm->{blob} =`git cat-file blob $gm->{B}`
+		if ($gm->{blob} eq '' && $gm->{B} ne '');
+	$gm->{blob} .= "[submodule \"$repo\"]\n";
+	$gm->{blob} .= "\tpath = $repo\n";
+	$gm->{blob} .= "\turl = http://llvm.org/git/$repo.git\n";
+	$gm->{B} = &sb_hash("git hash-object -w --stdin", $gm->{blob});
+	$subm->{'.gitmodules'} = $gm;
+}
 
 sub make_commit {
 	my ($tree, $msg, $parent) = @_;
 	my $mktree = sub {
 		my ($F) = @_;
 		for my $repo (sort keys %$tree) {
-			print $F "$tree->{$repo}{A} $tree->{$repo}{O} $tree->{$repo}{H}\t$repo\n";
+			if ($tree->{$repo}{B} ne '') {
+				print $F "100644 blob $tree->{$repo}{B}\t$repo\n";
+			} elsif ($tree->{$repo}{T} ne '') {
+				print $F "040000 tree $tree->{$repo}{T}\t$repo\n";
+			} elsif ($tree->{$repo}{H} ne '') {
+				print $F "160000 commit $tree->{$repo}{H}\t$repo\n";
+			}
 		}
 	};
-	my $tree_hash = &sb_hash("git mktree", $mktree);
-	my $com_tree = sub {
-		my ($F) = @_;
-		print $F $msg;
-	};
+	my @tree = &subprocess("git mktree", $mktree);
+	my $tree_hash = shift @tree;
+	chomp $tree_hash;
 	if ($parent ne '') {
 		$parent = "-p $parent";
 	}
-	return &sb_hash("git commit-tree $tree_hash $parent", $com_tree);
+	return &sb_hash("git commit-tree $tree_hash $parent", $msg);
 }
 
 sub subprocess {
@@ -172,15 +249,20 @@ sub subprocess {
 }
 
 sub sb_hash {
-	my @r = &subprocess(@_);
+	my ($cmd, $stdin) = @_;
+	my $cb = sub {
+		my $F = shift @_;
+		print $F $stdin;
+	};
+	my @r = &subprocess($cmd, $cb);
 	my $r = shift @r;
 	chomp $r;
 	return $r;
 }
 
 sub get_commits2 {
-	my ($h, $min) = @_;
-	my $commits = {};
+	my ($commits, $min, $h) = @_;
+	return if $min eq $h;
 	if ($min ne '') {
 		$h = "$min..$h";
 	}
@@ -200,7 +282,6 @@ sub get_commits2 {
 		$t = &parse_commit(@cm);
 		$commits->{$t->{REV}} = $t;
 	}
-	return $commits;
 }
 
 sub parse_commit {
